48ea588848719df4964f982fd93b62d32cd5cb0b
Andrew <andrewnguyena3@gmail.com>
2025-06-11 17:37:04 -0700

    Hello World Template

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-import json
 import os
 import requests
-from bs4 import BeautifulSoup
 import random
 import time
 import pandas as pd
 from datetime import datetime
 import math
-import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
